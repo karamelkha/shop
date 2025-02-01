1546c34749ee0a07e6d504cb30f80440e1e8971a
karamelkha <khalturinaks@gmail.com>
2025-02-01 16:28:04 +0300

    input, btn

--- shop/src/App.tsx
+++ shop/src/App.tsx
@@ -1,5 +1,22 @@
+import { MouseEvent } from "react";
+import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
 function App() {
-  return <></>;
+  // const [counter, setCounter] = useState<number>(0);
+  const addCounter = (e: MouseEvent) => {
+    console.log(e);
+  };
+  return (
+    <>
+      <Button appearence="big" onClick={addCounter}>
+        Big
+      </Button>
+      <Button appearence="small" onClick={addCounter}>
+        small
+      </Button>
+      <Input></Input>
+    </>
+  );
 }
 
 export default App;
--- shop/src/components/Button/Button.tsx
+++ shop/src/components/Button/Button.tsx
@@ -2,10 +2,18 @@ import { ButtonProps } from "./Button.props";
 import styles from "./Button.module.css";
 import cn from "classnames";
 
-function Button({ children, className, ...props }: ButtonProps) {
+function Button({
+  children,
+  appearence = "small",
+  className,
+  ...props
+}: ButtonProps) {
   return (
     <button
-      className={cn(styles["button"], styles["accent"], className)}
+      className={cn(styles["button"], styles["accent"], className, {
+        [styles["small"]]: appearence === "small",
+        [styles["big"]]: appearence === "big",
+      })}
       {...props}
     >
       {children}
