ef1c29f753a9f8b7194f973fb72ef1ffcd8803cf
karamelkha <khalturinaks@gmail.com>
2025-02-04 16:58:22 +0300

    route

--- shop/src/App.tsx
+++ shop/src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent } from "react";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
+import { Route, Routes } from "react-router-dom";
+import { Menu } from "./pages/Menu/Menu";
+import { Card } from "./pages/Card/Card";
+import { Error } from "./pages/Error/Error";
 function App() {
   // const [counter, setCounter] = useState<number>(0);
   const addCounter = (e: MouseEvent) => {
@@ -14,7 +18,16 @@ function App() {
       <Button appearence="small" onClick={addCounter}>
         small
       </Button>
-      <Input></Input>
+      <Input placeholder="Email" />
+      <div>
+        <a href="/">Меню</a>
+        <a href="/card">Корзина</a>
+      </div>
+      <Routes>
+        <Route path="/" element={<Menu />}></Route>
+        <Route path="/card" element={<Card />}></Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
     </>
   );
 }
