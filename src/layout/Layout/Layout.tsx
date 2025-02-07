5358814f9742c21fd25a5cc2e22f5abb727ed6ec
karamelkha <khalturinaks@gmail.com>
2025-02-07 15:37:54 +0300

    uselocation link sidebar

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,8 +1,15 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import styles from "./Layout.module.css";
 import Button from "../../components/Button/Button";
+import { useEffect } from "react";
+import cn from "classnames";
 
 export function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
   return (
     <div className={styles["layout"]}>
       <div className={styles["sidebar"]}>
@@ -16,11 +23,21 @@ export function Layout() {
           <div className={styles["email"]}>email</div>
         </div>
         <div className={styles["menu"]}>
-          <Link to="/" className={styles["link"]}>
+          <Link
+            to="/"
+            className={cn(styles["link"], {
+              [styles.active]: location.pathname == "/",
+            })}
+          >
             <img src="/menu-icon.svg" alt="Иконка меню" />
             Меню
           </Link>
-          <Link to="/cart" className={styles["link"]}>
+          <Link
+            to="/card"
+            className={cn(styles["link"], {
+              [styles.active]: location.pathname == "/card",
+            })}
+          >
             <img src="/cart-icon.svg" alt="Иконка корзины" />
             Корзина
           </Link>
