e269bdc50e2d329edb003b7956db7382c62f0346
karamelkha <khalturinaks@gmail.com>
2025-02-07 16:03:33 +0300

    active link sidebar

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,15 +1,10 @@
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import styles from "./Layout.module.css";
 import Button from "../../components/Button/Button";
-import { useEffect } from "react";
+
 import cn from "classnames";
 
 export function Layout() {
-  const location = useLocation();
-
-  useEffect(() => {
-    console.log(location);
-  }, [location]);
   return (
     <div className={styles["layout"]}>
       <div className={styles["sidebar"]}>
@@ -23,24 +18,28 @@ export function Layout() {
           <div className={styles["email"]}>email</div>
         </div>
         <div className={styles["menu"]}>
-          <Link
+          <NavLink
             to="/"
-            className={cn(styles["link"], {
-              [styles.active]: location.pathname == "/",
-            })}
+            className={({ isActive }) =>
+              cn(styles["link"], {
+                [styles.active]: isActive,
+              })
+            }
           >
             <img src="/menu-icon.svg" alt="Иконка меню" />
             Меню
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/card"
-            className={cn(styles["link"], {
-              [styles.active]: location.pathname == "/card",
-            })}
+            className={({ isActive }) =>
+              cn(styles["link"], {
+                [styles.active]: isActive,
+              })
+            }
           >
             <img src="/cart-icon.svg" alt="Иконка корзины" />
             Корзина
-          </Link>
+          </NavLink>
         </div>
         <Button className={styles["exit"]}>
           <img src="/exit-icon.svg" alt="Иконка выхода" />
