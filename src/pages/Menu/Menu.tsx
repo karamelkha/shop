9d43b0c3c238657369634c08a892178333a250e1
karamelkha <khalturinaks@gmail.com>
2025-02-09 15:11:00 +0300

    axios

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -5,22 +5,30 @@ import Search from "../../components/Search/Search";
 import { PREFIX } from "../../helpers/API";
 import { Product } from "../../interfaces/product.interface";
 import styles from "./Menu.module.css";
+import axios from "axios";
 
 export function Menu() {
   const [products, setProducts] = useState<Product[]>([]);
 
   const getMenu = async () => {
-    const res = await fetch(`${PREFIX}`);
     try {
-      if (!res.ok) {
-        return;
-      }
-      const data = (await res.json()) as Product[];
+      const { data } = await axios.get<Product[]>(`${PREFIX}`);
       setProducts(data);
     } catch (e) {
       console.error(e);
       return;
     }
+    // try {
+    //   const res = await fetch(`${PREFIX}`);
+    //   if (!res.ok) {
+    //     return;
+    //   }
+    //   const data = (await res.json()) as Product[];
+    //   setProducts(data);
+    // } catch (e) {
+    //   console.error(e);
+    //   return;
+    // }
   };
 
   useEffect(() => {
