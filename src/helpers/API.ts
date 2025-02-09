432e8733d629a2d05f0d49e495b3011e69b34470
karamelkha <khalturinaks@gmail.com>
2025-02-09 14:22:05 +0300

    data products

---
+++ src/helpers/API.ts
@@ -0,0 +1,1 @@
+export const PREFIX = './data/products.json';
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import Headling from "../../components/Headling/Headling";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
+import { PREFIX } from "../../helpers/API";
+import { Product } from "../../interfaces/product.interface";
 import styles from "./Menu.module.css";
 
 export function Menu() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getMenu = async () => {
+    const res = await fetch(`${PREFIX}`);
+    try {
+      if (!res.ok) {
+        return;
+      }
+      const data = (await res.json()) as Product[];
+      setProducts(data);
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
   return (
     <div>
       <div className={styles["head"]}>
@@ -11,14 +34,16 @@ export function Menu() {
         <Search placeholder="Введите блюдо или состав" />
       </div>
       <div>
-        <ProductCard
-          id={1}
-          title="Наслаждение"
-          description="Салями, руккола, помидоры, оливки"
-          rating={4.5}
-          price={300}
-          image="./product-image.png"
-        ></ProductCard>
+        {products.map((p) => (
+          <ProductCard
+            id={p.id}
+            name={p.name}
+            description={p.ingredients.join(", ")}
+            rating={p.rating}
+            price={p.price}
+            image={p.image}
+          />
+        ))}
       </div>
     </div>
   );
