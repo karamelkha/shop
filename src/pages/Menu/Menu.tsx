9c163606a05264be426353a7c24092b0aef9a89a
karamelkha <khalturinaks@gmail.com>
2025-02-18 16:12:00 +0300

    error

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import Headling from "../../components/Headling/Headling";
-import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
 import { PREFIX } from "../../helpers/API";
 import { Product } from "../../interfaces/product.interface";
 import styles from "./Menu.module.css";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { MenuList } from "./MenuList/MeniList";
 
 export function Menu() {
   const [products, setProducts] = useState<Product[]>([]);
-
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | undefined>();
 
   const getMenu = async () => {
     try {
@@ -25,6 +25,10 @@ export function Menu() {
       setIsLoading(false);
     } catch (e) {
       console.error(e);
+      if (e instanceof AxiosError) {
+        setError(e.message);
+      }
+      setIsLoading(false);
       return;
     }
   };
@@ -40,17 +44,8 @@ export function Menu() {
         <Search placeholder="Введите блюдо или состав" />
       </div>
       <div>
-        {!isLoading &&
-          products.map((p) => (
-            <ProductCard
-              id={p.id}
-              name={p.name}
-              description={p.ingredients.join(", ")}
-              rating={p.rating}
-              price={p.price}
-              image={p.image}
-            />
-          ))}
+        {error && <>{error}</>}
+        {!isLoading && <MenuList products={products} />}
         {isLoading && <>Загрузка</>}
       </div>
     </div>
---
+++ src/pages/Menu/MenuList/MeniList.tsx
@@ -0,0 +1,14 @@
+import ProductCard from "../../../components/ProductCard/ProductCard";
+import { MenuListProps } from "./MenuList.props";
+export function MenuList({ products }: MenuListProps) {
+  return products.map((p) => (
+    <ProductCard
+      id={p.id}
+      name={p.name}
+      description={p.ingredients.join(", ")}
+      rating={p.rating}
+      price={p.price}
+      image={p.image}
+    />
+  ));
+}
